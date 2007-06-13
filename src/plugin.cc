c465f8f2a1f221ee4f44da1e2cdb288897079c7a
cmsbuild <>
2007-06-13 13:11:40 +0000

    This commit was manufactured by cvs2git to create tag 'CondCore- SiStripPlugins-SiStrip_x140_V0'.
    Sprout from master 2005-11-29 12:12:48 UTC Giacomo Bruno <sha1-ff4a636444b84e278248a8d7454b0d8fb4ffd44c@cern.ch> '*** empty log message ***'
    Cherrypick from master 2007-06-13 13:11:39 UTC Domenico Giordano <sha1-07f7bb78311ae556a5edf2d08855b34552c86a25@cern.ch> 'added SiStripBadStrip SiStripModuleHV':
        src/plugin.cc

--- src/plugin.cc
+++ src/plugin.cc
@@ -7,15 +7,27 @@
  */
 
 #include "CondCore/PluginSystem/interface/registration_macros.h"
-#include "CondFormats/TrackerMapping/interface/SiStripReadOutCabling.h"
-#include "CondFormats/TrackerMapping/interface/SiStripControlCabling.h"
-#include "CondFormats/DataRecord/interface/SiStripReadOutCablingRcd.h"
-#include "CondFormats/DataRecord/interface/SiStripControlCablingRcd.h"
 #include "CondFormats/SiStripObjects/interface/SiStripPedestals.h"
 #include "CondFormats/DataRecord/interface/SiStripPedestalsRcd.h"
-
+#include "CondFormats/SiStripObjects/interface/SiStripNoises.h"
+#include "CondFormats/DataRecord/interface/SiStripNoisesRcd.h"
+#include "CondFormats/SiStripObjects/interface/SiStripFedCabling.h"
+#include "CondFormats/DataRecord/interface/SiStripFedCablingRcd.h"
+#include "CondFormats/SiStripObjects/interface/SiStripLorentzAngle.h"
+#include "CondFormats/DataRecord/interface/SiStripLorentzAngleRcd.h"
+#include "CondFormats/SiStripObjects/interface/SiStripApvGain.h"
+#include "CondFormats/DataRecord/interface/SiStripApvGainRcd.h"
+#include "CondFormats/SiStripObjects/interface/SiStripBadStrip.h"
+#include "CondFormats/DataRecord/interface/SiStripBadStripRcd.h"
+#include "CondFormats/SiStripObjects/interface/SiStripModuleHV.h"
+#include "CondFormats/DataRecord/interface/SiStripModuleHVRcd.h"
 
 DEFINE_SEAL_MODULE();
-REGISTER_PLUGIN(SiStripReadOutCablingRcd, SiStripReadOutCabling);
-REGISTER_PLUGIN(SiStripControlCablingRcd, SiStripControlCabling);
 REGISTER_PLUGIN(SiStripPedestalsRcd,SiStripPedestals);
+REGISTER_PLUGIN(SiStripNoisesRcd,SiStripNoises);
+REGISTER_PLUGIN(SiStripFedCablingRcd,SiStripFedCabling);
+REGISTER_PLUGIN(SiStripLorentzAngleRcd,SiStripLorentzAngle);
+REGISTER_PLUGIN(SiStripApvGainRcd,SiStripApvGain);
+REGISTER_PLUGIN(SiStripBadStripRcd,SiStripBadStrip);
+REGISTER_PLUGIN(SiStripModuleHVRcd,SiStripModuleHV);
+
